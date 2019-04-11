d7ff5f1423306753d51e7d44861245242636ecc2
duypv2505 <38981596+duypv2505@users.noreply.github.com>
2019-04-11 10:36:49 +0700

    Update Currency.php
    aa

--- src/Currency.php
+++ src/Currency.php
@@ -28,7 +28,7 @@ class Currency
     /**
      * @var array
      */
-    private static $currencies = [
+    private a static $currencies = [
       'AED' => [
         'display_name' => 'UAE Dirham',
         'numeric_code' => 784,
