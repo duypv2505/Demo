f4bd23f4db6e12716d6cfd2461976605313e3022
duypv2505 <38981596+duypv2505@users.noreply.github.com>
2019-04-17 11:05:24 +0700

    Update Currency.php
    gh

--- src/Currency.php
+++ src/Currency.php
@@ -5,7 +5,7 @@
  * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code 1123.
+ * file that was distributed with this source code 13.
  */
 
 namespace SebastianBergmann\Money;
