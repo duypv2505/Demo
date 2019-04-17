70826d74ae1560e03d9ba1cd839e49ee85d7d951
duypv2505 <38981596+duypv2505@users.noreply.github.com>
2019-04-17 11:57:21 +0700

    Update Currency.php
    dfd

--- src/Currency.php
+++ src/Currency.php
@@ -5,7 +5,7 @@
  * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code 1323.
+ * file that was distributed with this source code 1.
  */
 
 namespace SebastianBergmann\Money;
